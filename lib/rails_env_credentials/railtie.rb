5fcdd9885fdfa30b6934a29bb92dd057a5538916
sinsoku <sinsoku.listy@gmail.com>
2018-02-10 23:24:59 +0900

    :pill: Keep the behavior of original `credentials:edit` command

--- lib/rails_env_credentials/railtie.rb
+++ lib/rails_env_credentials/railtie.rb
@@ -3,7 +3,8 @@
 module RailsEnvCredentials
   class Railtie < ::Rails::Railtie
     initializer 'rails-env-credentials' do
-      Rails::Application.prepend(CredentialsOverwrite)
+      is_credentials_command = Rails::Command.const_defined?(:CredentialsCommand) && !Rails::Command.const_defined?(:EnvCredentialsCommand)
+      Rails::Application.prepend(CredentialsOverwrite) unless is_credentials_command
     end
   end
 end
