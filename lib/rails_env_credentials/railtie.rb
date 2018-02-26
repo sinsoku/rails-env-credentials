8c28ec9682fb8caa9c9c196b801c908a07740a01
sinsoku <sinsoku.listy@gmail.com>
2018-02-26 20:29:48 +0900

    :pill: Fix a bug that cannot execute rake tasks
    It occurred NameError(uninitialized constant Rails::Command) when the rake task executes.

--- lib/rails_env_credentials/railtie.rb
+++ lib/rails_env_credentials/railtie.rb
@@ -3,7 +3,9 @@
 module RailsEnvCredentials
   class Railtie < ::Rails::Railtie
     initializer 'rails-env-credentials' do
-      is_credentials_command = Rails::Command.const_defined?(:CredentialsCommand) && !Rails::Command.const_defined?(:EnvCredentialsCommand)
+      is_credentials_command = Rails.const_defined?(:Command) &&
+        Rails::Command.const_defined?(:CredentialsCommand) &&
+        !Rails::Command.const_defined?(:EnvCredentialsCommand)
       Rails::Application.prepend(CredentialsOverwrite) unless is_credentials_command
     end
   end
