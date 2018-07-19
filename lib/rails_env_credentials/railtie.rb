321ef0444a348fce7268c8916b65f9308dda4b7e
Rick Song <rickcsong@gmail.com>
2018-07-19 12:32:53 -0700

    Ensure that CredentialsOverwrite occurs before_configuration
    This is critical because otherwise, credentials accessed in
    initializers/application.rb will default to the production
    credentials.
    
    By running this in before_configuration, we ensure RailsEnvCredentials
    is properly prepended before anything in application.rb
    occurs or any initializers.

--- lib/rails_env_credentials/railtie.rb
+++ lib/rails_env_credentials/railtie.rb
@@ -2,10 +2,11 @@
 
 module RailsEnvCredentials
   class Railtie < ::Rails::Railtie
-    initializer 'rails-env-credentials' do
+    config.before_configuration do
       is_credentials_command = Rails.const_defined?(:Command) &&
         Rails::Command.const_defined?(:CredentialsCommand) &&
         !Rails::Command.const_defined?(:EnvCredentialsCommand)
+
       Rails::Application.prepend(CredentialsOverwrite) unless is_credentials_command
     end
   end
